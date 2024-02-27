47f31cbe1ca64600137c2816d12398559d2a313a
Bartosz Stanczuk <synergy170185@gmail.com>
2024-02-27 16:45:30 +0000

    add last button and turn in progress, polish and finish game - completing javaScript testing with jest

--- scripts/test/game.test.js
+++ scripts/test/game.test.js
@@ -40,6 +40,15 @@ describe("game object contains correct keys", () => {
     test("turnNumber key exist", () => {
         expect("turnNumber" in game).toBe(true);
     });
+    test("last button key exist", () => {
+        expect("lastButton" in game).toBe(true);
+    });
+    test("turnInProgress key exist", ()=> {
+        expect("turnInProgress" in game).toBe(true);
+    });
+    test("turnInProgress key value is false", ()=> {
+        expect("turnInProgress" in game).toBe(true);
+    });
 
 });
 
@@ -108,4 +117,16 @@ describe("gameplay works corrctly", () => {
         playerTurn();
         expect(window.alert).toBeCalledWith("Wrong Move");
     });
+    test("should toggle turnInProgress to true", () => {
+        showTurns();
+        expect(game.turnInProgress).toBe(true);
+    });
+    test("clicking during computer sequesnce should fail", () => {
+        showTurns();
+        game.lastButton = "";
+        document.getElementById("button2").click();
+        expect(game.lastButton).toEqual("");
+
+    });
+    
 });
